cf0da1184ec4d147673a6b1c1a5e572e75ac6106
Rafael Dametto <cacotpds@gmail.com>
2023-08-16 10:59:29 -0300

    Criando rotas

--- react-router-dom/src/main.tsx
+++ react-router-dom/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "../src/components/styles/global.css";
 import { Home } from "./components/Home";
+import { About } from "./components/About";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
